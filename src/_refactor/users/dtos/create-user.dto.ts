c3daf7093763b63e80fad2960af2b87597e26805
h3nrzi <rezaeig22@gmail.com>
2025-07-12 12:09:00 +0330

    refactor: implement user repository and data transfer objects for user creation and update

---
+++ src/_refactor/users/dtos/create-user.dto.ts
@@ -0,0 +1,6 @@
+export interface ICreateUserDto {
+	name: string;
+	email: string;
+	password: string;
+	passwordConfirmation: string;
+}
---
+++ src/_refactor/users/user.repository.ts
@@ -0,0 +1,32 @@
+import { IUserDoc, IUserModel } from "./interfaces/user.interface";
+import { ICreateUserDto } from "./dtos/create-user.dto";
+import { IUpdateUserDto } from "./dtos/update-user.dto";
+
+export class UserRepository {
+	constructor(private readonly userModel: IUserModel) {}
+
+	async findAll(): Promise<IUserDoc[]> {
+		return this.userModel.find();
+	}
+
+	async findById(userId: string): Promise<IUserDoc | null> {
+		return this.userModel.findById(userId);
+	}
+
+	async create(createUserDto: ICreateUserDto): Promise<IUserDoc> {
+		return this.userModel.create(createUserDto);
+	}
+
+	async update(
+		userId: string,
+		updateUserDto: IUpdateUserDto
+	): Promise<IUserDoc | null> {
+		return this.userModel.findByIdAndUpdate(userId, updateUserDto, {
+			new: true,
+		});
+	}
+
+	async delete(userId: string): Promise<IUserDoc | null> {
+		return this.userModel.findByIdAndDelete(userId);
+	}
+}
