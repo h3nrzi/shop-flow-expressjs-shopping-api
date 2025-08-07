4b18f9e1da722b785d65c1995fbfd86dbe0f6d04
h3nrzi <rezaeig22@gmail.com>
2025-08-07 23:24:13 +0330

    chore

--- src/core/products/product.routes.ts
+++ src/core/products/product.routes.ts
@@ -9,10 +9,7 @@ const router = Router();
 
 router.use("/:productId/reviews", reviewRouter);
 
-router.get(
-	"/",
-	productController.getAllProducts.bind(productController)
-);
+router.get("/", productController.getAllProducts.bind(productController));
 
 router.get(
 	"/:id",
@@ -29,54 +26,21 @@ router.use(authMiddleware.restrictTo("admin"));
 
 router.post(
 	"/",
-	body("name")
-		.exists({ checkFalsy: true })
-		.isString()
-		.withMessage("نام محصول الزامی است"),
-	body("description")
-		.exists({ checkFalsy: true })
-		.isString()
-		.withMessage("توضیحات محصول الزامی است"),
-	body("image")
-		.exists({ checkFalsy: true })
-		.isString()
-		.withMessage("تصویر محصول الزامی است"),
-	body("images")
-		.optional()
-		.isArray()
-		.withMessage("تصویرهای محصول باید آرایه باشد"),
-	body("countInStock")
-		.optional()
-		.isInt({ min: 0 })
-		.withMessage("تعداد محصولات باید عدد صحیح و مثبت باشد"),
-	body("isAvailable")
-		.optional()
-		.isBoolean()
-		.withMessage("وضعیت محصول باید بولین باشد"),
-	body("brand")
-		.exists({ checkFalsy: true })
-		.isString()
-		.withMessage("برند محصول الزامی است"),
-	body("category")
-		.exists({ checkFalsy: true })
-		.isString()
-		.withMessage("دسته بندی محصول الزامی است"),
-	body("rating")
-		.optional()
-		.isNumeric()
-		.withMessage("امتیاز محصول باید عددی باشد"),
-	body("numReviews")
-		.optional()
-		.isInt({ min: 0 })
-		.withMessage("تعداد بازخوردها باید عدد صحیح و مثبت باشد"),
-	body("price")
-		.exists({ checkFalsy: true })
-		.isNumeric()
-		.withMessage("قیمت محصول الزامی است"),
-	body("discount")
-		.optional()
-		.isNumeric()
-		.withMessage("تخفیف محصول باید عددی باشد"),
+	body("name").exists({ checkFalsy: true }).isString().withMessage("نام محصول الزامی است"),
+	body("description").exists({ checkFalsy: true }).isString().withMessage("توضیحات محصول الزامی است"),
+	body("image").exists({ checkFalsy: true }).isString().withMessage("تصویر محصول الزامی است"),
+	// body("images")
+	// 	.optional()
+	// 	.isArray()
+	// 	.withMessage("تصویرهای محصول باید آرایه باشد"),
+	body("countInStock").optional().isInt({ min: 0 }).withMessage("تعداد محصولات باید عدد صحیح و مثبت باشد"),
+	body("isAvailable").optional().isBoolean().withMessage("وضعیت محصول باید بولین باشد"),
+	body("brand").exists({ checkFalsy: true }).isString().withMessage("برند محصول الزامی است"),
+	body("category").exists({ checkFalsy: true }).isString().withMessage("دسته بندی محصول الزامی است"),
+	body("rating").optional().isNumeric().withMessage("امتیاز محصول باید عددی باشد"),
+	body("numReviews").optional().isInt({ min: 0 }).withMessage("تعداد بازخوردها باید عدد صحیح و مثبت باشد"),
+	body("price").exists({ checkFalsy: true }).isNumeric().withMessage("قیمت محصول الزامی است"),
+	body("discount").optional().isNumeric().withMessage("تخفیف محصول باید عددی باشد"),
 	validateRequest,
 	productController.createProduct.bind(productController)
 );
@@ -84,51 +48,18 @@ router.post(
 router.patch(
 	"/:id",
 	param("id").isMongoId().withMessage("شناسه محصول معتبر نیست"),
-	body("name")
-		.optional()
-		.isString()
-		.withMessage("نام محصول الزامی است"),
-	body("description")
-		.optional()
-		.isString()
-		.withMessage("توضیحات محصول الزامی است"),
-	body("image")
-		.optional()
-		.isString()
-		.withMessage("تصویر محصول الزامی است"),
+	body("name").optional().isString().withMessage("نام محصول الزامی است"),
+	body("description").optional().isString().withMessage("توضیحات محصول الزامی است"),
+	body("image").optional().isString().withMessage("تصویر محصول الزامی است"),
 	// body("images").optional().isArray().withMessage("تصویرهای محصول الزامی است"),
-	body("countInStock")
-		.optional()
-		.isInt({ min: 0 })
-		.withMessage("تعداد محصولات الزامی است"),
-	body("isAvailable")
-		.optional()
-		.isBoolean()
-		.withMessage("وضعیت محصول الزامی است"),
-	body("brand")
-		.optional()
-		.isString()
-		.withMessage("برند محصول الزامی است"),
-	body("category")
-		.optional()
-		.isString()
-		.withMessage("دسته بندی محصول الزامی است"),
-	body("rating")
-		.optional()
-		.isNumeric()
-		.withMessage("امتیاز محصول الزامی است"),
-	body("numReviews")
-		.optional()
-		.isInt({ min: 0 })
-		.withMessage("تعداد بازخوردها محصول الزامی است"),
-	body("price")
-		.optional()
-		.isNumeric()
-		.withMessage("قیمت محصول الزامی است"),
-	body("discount")
-		.optional()
-		.isNumeric()
-		.withMessage("تخفیف محصول الزامی است"),
+	body("countInStock").optional().isInt({ min: 0 }).withMessage("تعداد محصولات الزامی است"),
+	body("isAvailable").optional().isBoolean().withMessage("وضعیت محصول الزامی است"),
+	body("brand").optional().isString().withMessage("برند محصول الزامی است"),
+	body("category").optional().isString().withMessage("دسته بندی محصول الزامی است"),
+	body("rating").optional().isNumeric().withMessage("امتیاز محصول الزامی است"),
+	body("numReviews").optional().isInt({ min: 0 }).withMessage("تعداد بازخوردها محصول الزامی است"),
+	body("price").optional().isNumeric().withMessage("قیمت محصول الزامی است"),
+	body("discount").optional().isNumeric().withMessage("تخفیف محصول الزامی است"),
 	validateRequest,
 	productController.updateProduct.bind(productController)
 );
